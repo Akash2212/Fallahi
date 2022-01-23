93735646f2647d3b9fb5f7c0d36e969b64f00776
Akash <38830577+Akash2212@users.noreply.github.com>
2022-01-23 19:24:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import Splash from './screens/Splash'
+import MainScreen from './screens/MainScreen'
+import EnterEmail from './screens/EnterEmail'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database'
+
+const Stack = createNativeStackNavigator();
+
+var user = auth().currentUser
+
+if (user == null) {
+    auth()
+        .signInAnonymously()
+        .then(() => {
+            console.log('User signed in anonymously');
+        })
+        .catch(error => {
+            if (error.code === 'auth/operation-not-allowed') {
+                console.log('Enable anonymous in your firebase console.');
+            }
+
+            console.error(error);
+        });
+}
+
+
+
+
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            component: <Splash />,
+        };
+        console.disableYellowBox = true
+    }
+
+
+    componentDidMount() {
+        this.timeoutHandle = setTimeout(() => {
+
+            if (user != null) {
+
+                database().ref('Users/' + user.uid)
+                    .once('value')
+                    .then(snapshot => {
+                        if (snapshot.exists()) {
+                            console.log('Exists')
+                            this.setState({ component: <MainScreen /> })
+                        }
+                        else {
+                            console.log("Does not exists")
+                            this.setState({
+                                component:
+                                    <NavigationContainer>
+                                        <Stack.Navigator screenOptions={{ headerShown: false }}  >
+                                            <Stack.Screen name="EnterEmail" component={EnterEmail} />
+                                            <Stack.Screen name="MainScreen" component={MainScreen} /></Stack.Navigator>
+                                    </NavigationContainer>
+                            })
+                        }
+                    })
+            }
+
+        }, 1000);
+
+
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timeoutHandle);
+    }
+
+
+
+    render() {
+        return (this.state.component)
+
+    }
+}
